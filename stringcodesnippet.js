796433fa9e9f1480697ce5dd25159ca53371d956
Kamal Rawat <kamalrawat86@hotmail.com>
2018-03-13 10:50:06 -0700

    Update stringcodesnippet.js

--- stringcodesnippet.js
+++ stringcodesnippet.js
@@ -9,3 +9,6 @@ var s   = str.match(/([a-zA-Z])\1*/g)||[];
 s.reduce(function(a,b){
   return a.length > b.length ? a : b;
 });
+
+Note: 
+\1* is used to allow matching instances of a single letter with any or no sequential repetition. '\1+' would not match a single unrepeated letter.
