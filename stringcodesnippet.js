75565d33f92562f569b810eba81dc822175cf9f9
Kamal Rawat <kamalrawat86@hotmail.com>
2018-03-13 11:18:52 -0700

    Update stringcodesnippet.js

--- stringcodesnippet.js
+++ stringcodesnippet.js
@@ -1,5 +1,4 @@
 #Write a code snippet to find consecuitve charstring in a string.
-
 Input : 'avcccceedffftvvvvvvvvvv'
 Output:  vvvvvvvvvv
 
@@ -12,3 +11,5 @@ s.reduce(function(a,b){
 
 Note: 
 \1* is used to allow matching instances of a single letter with any or no sequential repetition. '\1+' would not match a single unrepeated letter.
+---------------------------------------------------------------------------------------------------------------------------------------
+#
